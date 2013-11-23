ed03301c39c32b1af887321959734ffaffb4cd40
Mohammad Razeghi <razeghi71@gmail.com>
2013-11-23 04:55:42 +0330

    compatibility to get input file from sys args

--- PlanetWars/Main.java
+++ PlanetWars/Main.java
@@ -11,10 +11,16 @@ public class Main {
 
     public static void main(String[] args) {
     	GraphicEngine engine = new GraphicalWorld();
-        
-        Game g = new Game(4000, "/resources/default.map", engine);
+        Game g = null;
+        if ( args.length == 1 )
+            g = new Game(4000, args[0], engine);
+        else{
+            System.err.println("usage : \n java -jar Planet.jar mapfile" );
+            System.exit(1);
+        }
+            
         System.out.println("Listening On Port 0.0.0.0:4000");
-        
+
         g.doSim();
     }
     
